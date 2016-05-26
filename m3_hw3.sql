ae9cffcf1f5e61169aa5c49180aec77def69b397
leonid <vasiliev.leonid@gmail.com>
2016-05-26 12:12:11 +0000

    amendments

--- m3_hw3.sql
+++ m3_hw3.sql
@@ -1,3 +1,3 @@
 CREATE TABLE conference (name text, description text, primary key (name));
-CREATE TABLE venue (id integer primary key, name text unique, city text, country text, lat numeric(7,5), lon numeric(8,5), unique (city, country, lat, lon));
-CREATE TABLE papersubmission(id integer primary key, conference text, year integer, title text, isbn text unique, page integer, venue_id integer, unique (conference, year, isbn), foreign key (conference) references conference (name), foreign key (venue_id) references venue (id));
+CREATE TABLE venue (id integer primary key, name text, city text, country text, lat numeric(7,5), lon numeric(8,5),unique(name,city,country), unique(lat,lon));
+CREATE TABLE papersubmission(id integer primary key, conference text, year integer, title text, isbn text, page integer, venue_id integer, unique (conference,year,venue_id), unique(id,page),unique (conference,year,isbn),foreign key (conference) references conference (name), foreign key (venue_id) references venue (id));
